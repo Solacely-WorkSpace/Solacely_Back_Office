758fd6a035d10825c4db4e7bb91d5d3698a50777
LazarusUno <alazarashebir7@gmail.com>
2025-07-23 19:00:56 +0300

    final fix

--- app/page.js
+++ app/page.js
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import ListingMapView from "./_components/ListingMapView";
-import Hero from "@/components/Hero";
-import Tour from "@/components/Tour";
-import Waiting from "@/components/Waiting";
-import Offer from "@/components/Offer";
-import Ad from "@/components/Ad";
-import Cta from "@/components/Cta";
-import Nav from '@/components/Nav'
-import Footer from '@/components/Footer'
+import {Hero }from "@/components/Hero";
+import {Tour} from "@/components/Tour";
+import {Waiting} from "@/components/Waiting";
+import {Offer} from "@/components/Offer";
+import {Ad} from "@/components/Ad";
+import {Cta} from "@/components/Cta";
+import {Nav} from '@/components/Nav'
+import {Footer} from '@/components/Footer'
 
 export default function Home() {
   return (
